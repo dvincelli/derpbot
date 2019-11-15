e5fc996784c9557cc9e470d9ff3c2d22739a29c0
David Vincelli <david.vincelli@gmail.com>
2019-11-14 22:52:31 -0500

    Py3 Fix

--- derp/command/dispatcher.py
+++ derp/command/dispatcher.py
@@ -34,12 +34,30 @@ class CommandDispatcher(object):
     def parse_bomb(self, message):
         return self.bomb_pattern.match(message['body']).groups()
 
-    def __call__(self, message):
+    def __call__(self, *args, **kwargs):
+        if len(args) == 1:
+            message = args[0]
+            message['from'] = message['from'].bare
+            message['to'] = message['to'].bare
+        else:
+            # {'rtm_client': <slack.rtm.client.RTMClient object at 0x7fb51778f860>, 'web_client': <slack.web.client.WebClient object at 0x7fb515cbca20>, 'data': {'client_msg_id': '91181cfe-5db8-4707-b5dd-90e101c5d68e', 'suppress_notification': False, 'text': 'hi', 'user': 'U04HF1BP9', 'team': 'T04EN147C', 'user_team': 'T04EN147C', 'source_team': 'T04EN147C', 'channel': 'DQA2P16G1', 'event_ts': '1573707936.000400', 'ts': '1573707936.000400'}}
+            try:
+                slack_message = kwargs['data']
+                message = {}
+                message['body'] = slack_message['text']
+                message['to'] =  slack_message['user']
+                message['from'] = slack_message['channel']
+                message['status'] = 'whatever'
+                message['type'] = 'who cares'
+            except KeyError:
+                print(kwargs)
+                return
+
         repeats = 1
         if self.is_bomb(message):
             repeats, command, etc = self.parse_bomb(message)
             message['body'] = command + etc
             repeats = min(int(repeats), 50)
-        for x in xrange(0, repeats):
+        for x in range(0, repeats):
             self.put(message)
 
