a7e46904b818b28023e6384ad3385a04ff366e60
David Vincelli <david.vincelli@gmail.com>
2023-05-16 20:47:10 -0400

    Fix tests

--- tests/commands/test_calc.py
+++ tests/commands/test_calc.py
@@ -2,27 +2,27 @@ from derp.commands.calc import CalculatorCommand, RPNCalculatorCommand
 
 def test_complex_formula():
     c = CalculatorCommand()
-    answer = c({'body': '!calc 2 * (2 * (4 + 6 + 1) + 1)'})
+    answer = c(['@bot', 'calc', {'expr': '2 * (2 * (4 + 6 + 1) + 1)'}])
     assert answer == '2 * (2 * (4 + 6 + 1) + 1) = 46', answer + ' should be 46'
 
 def test_complex_rpn_formula():
     c = RPNCalculatorCommand()
-    answer = c({'body': '!rpn 2 2 8 6 + 1 + * 1 + *'})
+    answer = c(['@bot', 'rpn', {'expr': '2 2 8 6 + 1 + * 1 + *'}])
     assert answer == '2 2 8 6 + 1 + * 1 + * = 62', answer + ' should be 62'
 
 def test_wikipedia_rpn_formula():
     c = RPNCalculatorCommand()
-    answer = c({'body': '!rpn 5 1 2 + 4 * + 3 -'})
+    answer = c(['@bot', 'calc', {'expr': '5 1 2 + 4 * + 3 -'}])
     assert answer == '5 1 2 + 4 * + 3 - = 14'
 
 def test_infix_to_postfix_simple_formula():
     c = CalculatorCommand()
-    answer = c({'body': '!calc (2 * (2 * 3 + 1))'})
+    answer = c(['@bot', 'calc', {'expr': '(2 * (2 * 3 + 1))'}])
     assert answer == '(2 * (2 * 3 + 1)) = 14'
 
 def test_infix_subtraction():
     c = CalculatorCommand()
-    answer = c({'body': '!calc 2 - 5'})
+    answer = c(['@bot', 'calc', {'expr': '2 - 5'}])
     assert answer == '2 - 5 = -3'
 
 
---
+++ tests/commands/test_image_command.py
@@ -0,0 +1,8 @@
+from derp.commands.images import ImageCommand
+
+
+
+def test_image_command():
+    msg = {'body': '!img test'}
+
+    ImageCommand()(msg)
